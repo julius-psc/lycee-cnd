d469e2c33df9bba10c8d2cf60d207e517e01ae03
julius-psc <peschardjulius03@gmail.com>
2023-05-07 16:13:56 +0200

    Desktop version final touches

--- src/components/_ABOUT/About.js
+++ src/components/_ABOUT/About.js
@@ -2,11 +2,24 @@
 
 
 import React from "react";
+import { useState } from "react";
+
 import { NavLink } from "react-router-dom";
 
 import '../../styles/_ABOUT/About.css';
 
 function About() {
+    const [educOpen, setEducOpen] = useState(true);
+    const [etabOpen, setEtabOpen] = useState(false);
+
+    const handleEducClick = () => {
+        setEducOpen(!educOpen);
+    };
+
+    const handleEtabClick = () => {
+        setEtabOpen(!etabOpen);
+    };
+
     return (
         <div className="about-sect">
             <h1>A Propos</h1>
@@ -17,37 +30,44 @@ function About() {
             </div>
 
             <div id="paragraph-container">
-                <h2>Projet d'éducation</h2>
+                <h2 onClick={handleEducClick}>Projet d'éducation<span className="text-dropdown">{educOpen ? " ▲" : " ▼"}</span></h2>
                 <p>L'équipe éducative a pour mission de former chaque élève en développant son savoir, sa culture et sa personnalité. Elle utilise des méthodes pédagogiques variées et favorise une ouverture d'esprit.</p>
-                <p>De plus, notre objectif est :</p>
-                <ul>
-                    <li><span>[+]</span>Faciliter la transition entre le collège et le lycée</li>
-                    <li><span>[+]</span>Enseigner des méthodes de travail efficaces</li>
-                    <li><span>[+]</span>Fournir un soutien aux élèves en difficulté</li>
-                    <li><span>[+]</span>Maintenir la motivation de tous les élèves</li>
-                    <li><span>[+]</span>Accompagner les élèves dans l'élaboration de leur projet personnel</li>
-                    <li><span>[+]</span>Encourager l'apprentissage de la liberté et de l'autonomie</li>
-                </ul>
+                {educOpen && (
+                    <>
+                        <p>De plus, notre objectif est :</p>
+                        <ul>
+                            <li><span>[+]</span>Faciliter la transition entre le collège et le lycée</li>
+                            <li><span>[+]</span>Enseigner des méthodes de travail efficaces</li>
+                            <li><span>[+]</span>Fournir un soutien aux élèves en difficulté</li>
+                            <li><span>[+]</span>Maintenir la motivation de tous les élèves</li>
+                            <li><span>[+]</span>Accompagner les élèves dans l'élaboration de leur projet personnel</li>
+                            <li><span>[+]</span>Encourager l'apprentissage de la liberté et de l'autonomie</li>
+                        </ul>
+                    </>
+                )}
             </div>
             
             <div id="paragraph-container">
-                <h2>Projet d'établissement</h2>
+                <h2 onClick={handleEtabClick}>Projet d'établissement<span className="text-dropdown">{etabOpen ? " ▲" : " ▼"}</span></h2>
                 <p>Au lycée Cours Notre-Dame, l'équipe éducative met en pratique des valeurs telles que le sens du travail, de la créativité, du respect et de l'autonomie. Elle encourage également l'esprit de solidarité, de responsabilité et d'engagement ainsi que l'écoute de l'autre et l'élaboration du projet personnel. Trois thèmes fondamentaux, basés sur ces valeurs, conditionnent la vie au lycée pour tous.</p>
+                {etabOpen && (
+                    <>
+                        <div id="sub-para">
+                            <h4>[1] Lieu de réussite</h4>
+                            <p>Nous aidons nos élèves à développer leurs compétences et à trouver leur orientation, y compris avec une section européenne pour l'accès aux grandes écoles et bourses Erasmus, horaire renforcé en anglais et voyage à Bruxelles pour l'acquisition d'une citoyenneté européenne. Nous offrons un soutien personnalisé et une classe préparatoire pour consolider les prérequis, avec une priorité sur la réussite de chaque élève.</p>
+                        </div>
 
-                <div id="sub-para">
-                    <h4>[1] Lieu de réussite</h4>
-                    <p>Nous aidons nos élèves à développer leurs compétences et à trouver leur orientation, y compris avec une section européenne pour l'accès aux grandes écoles et bourses Erasmus, horaire renforcé en anglais et voyage à Bruxelles pour l'acquisition d'une citoyenneté européenne. Nous offrons un soutien personnalisé et une classe préparatoire pour consolider les prérequis, avec une priorité sur la réussite de chaque élève.</p>
-                </div>
+                        <div id="sub-para">
+                            <h4>[2] Lieu d'ouverture</h4>
+                            <p>Nous valorisons notre lycée pour sa promotion de la vie culturelle et sportive, avec des visites enrichissantes au musée, au cinéma ou au théâtre pour développer un regard critique, des options Théâtre ou Arts plastiques pour une formation pratique et théorique, et des voyages et échanges linguistiques pour favoriser l'ouverture internationale et la compréhension d'autres cultures. Nous proposons également une pratique sportive avec la section voile ou l'option handball pour développer l'esprit d'initiative, le sens du groupe, et améliorer les compétences techniques, pouvant même faire l'objet d'épreuves au baccalauréat.</p>
+                        </div>
 
-                <div id="sub-para">
-                    <h4>[2] Lieu d'ouverture</h4>
-                    <p>Nous valorisons notre lycée pour sa promotion de la vie culturelle et sportive, avec des visites enrichissantes au musée, au cinéma ou au théâtre pour développer un regard critique, des options Théâtre ou Arts plastiques pour une formation pratique et théorique, et des voyages et échanges linguistiques pour favoriser l'ouverture internationale et la compréhension d'autres cultures. Nous proposons également une pratique sportive avec la section voile ou l'option handball pour développer l'esprit d'initiative, le sens du groupe, et améliorer les compétences techniques, pouvant même faire l'objet d'épreuves au baccalauréat.</p>
-                </div>
-
-                <div id="sub-para">
-                    <h4>[3] Lieu de vie</h4>
-                    <p>Notre lycée est situé à 10 minutes de Caen, proche de la mer et dans un parc classé "Monuments Historiques". Nous proposons un internat pour filles et garçons en chambres individuelles dans un cadre de vie agréable à taille humaine, avec des activités extrascolaires telles que des sorties culturelles et sportives pour encourager l'autonomie et le développement citoyen des élèves. Les terminales ont un statut spécifique pour renforcer leur autonomie, tandis que le Trophée des Lycées et le Bureau des élèves favorisent la prise de responsabilités.</p>
-                </div>
+                        <div id="sub-para">
+                            <h4>[3] Lieu de vie</h4>
+                            <p>Notre lycée est situé à 10 minutes de Caen, proche de la mer et dans un parc classé "Monuments Historiques". Nous proposons un internat pour filles et garçons en chambres individuelles dans un cadre de vie agréable à taille humaine, avec des activités extrascolaires telles que des sorties culturelles et sportives pour encourager l'autonomie et le développement citoyen des élèves. Les terminales ont un statut spécifique pour renforcer leur autonomie, tandis que le Trophée des Lycées et le Bureau des élèves favorisent la prise de responsabilités.</p>
+                        </div>
+                    </>
+                )}
             </div>
 
             <div className="container-wrapper">
--- src/components/_MAIN/CallToAction.js
+++ src/components/_MAIN/CallToAction.js
@@ -19,7 +19,7 @@ const CallToAction = () => {
             <div className="txt-section">
                 <h1>Qui nous sommes</h1>
                 <p>Le Lycée Cours Notre Dame est un établissement d'enseignement privé catholique situé à Douvres la Délivrande, en Normandie. Fondé en 1946, il accueille des élèves de la seconde à la terminale et propose des filières générales (L, ES, S) ainsi qu'une section STMG. <span>Le CND favorise la formation intégrale de ses élèves, en leur proposant des activités artistiques, culturelles, sportives et spirituelles.</span></p>
-                <NavLink to="/about"><button>A Propos</button></NavLink>
+                <NavLink to="/a-propos"><button>A Propos</button></NavLink>
             </div>
         </section>
     )
